d535ada4175c6c45674b5772013ca1cdac4c4948
juan-dev <juan-dev@pop-os.localdomain>
2023-01-30 19:18:42 -0300

    Icaro practico 1

--- index.js
+++ index.js
@@ -8,12 +8,9 @@ function _sum(x, y){
 
 x = 2;
 y = 5;
-let resultado = sum(x, y);
-
 _x = 23;
 _y = 232;
-let _resultado = sum(_y, _x);
 
 
-console.log("resultado de sumar", x,  "+", y, "=", resultado);
-console.log("resultado de sumar", _x, "+",  _y, "=", _resultado);
+console.log("resultado de sumar", x,  "+", y, "=", sum(_y, _x));
+console.log("resultado de sumar", _x, "+",  _y, "=", sum(x, y));
