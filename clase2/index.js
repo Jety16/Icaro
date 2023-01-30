518115e32aa741cc77e3fe0ed721997e0cb8e60c
juan-dev <juan-dev@pop-os.localdomain>
2023-01-30 19:21:26 -0300

    fix structure

---
+++ clase2/index.js
@@ -0,0 +1,24 @@
+let nombre = "el pepe";
+let appelido = "ETE SETCH";
+let edad = 20900;
+
+
+function concatValues(){
+    const palabraCompleta = nombre + ' ' + appelido + ' ' + edad;
+    return palabraCompleta
+}
+
+
+console.log(concatValues())
+
+
+
+self._square(x_1 + + 28, y_1 , length)
+self._square(x_2 + + 28, y_2 , length)
+self.oled.line(x_2 + + 28, y_2 , x_2 + + 28, y_2 , 1)
+self.oled.line(x_1 + length + + 28, y_1 ,
+               x_2 + length + + 28, y_2 , 1)
+self.oled.line(x_1 + + 28, y_1 + length ,
+               x_2 + + 28, y_2 + length , 1)
+self.oled.line(x_1 + length + + 28, y_1 + length +
+               48, x_2 + length + + 28, y_2 + length , 1)
