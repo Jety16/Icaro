9946fd1568e02605aa6d33abbcb70d8b0e7d761e
Juan Cruz Pereyra Carrillo <62359758+Jety16@users.noreply.github.com>
2023-01-30 19:25:25 -0300

    Update index.js

--- clases/clase1/index.js
+++ clases/clase1/index.js
@@ -1,13 +1,3 @@
 let myName = 'El pepe';
 
 console.log(myName);
-
-
-
-               self._square(x_1 + 40, y_1 + 40, length)
-                self._square(x_2 + 40, y_2 + 40, length)
-                self.oled.line(x_2 + 40, y_2 + 40, x_2 + 40, y_2 + 40, 1)
-                self.oled.line(x_1+length + 40, y_1 + 40, x_2+length + 40, y_2 + 40, 1)
-                self.oled.line(x_1 + 40, y_1+length + 40, x_2 + 40, y_2+length + 40, 1)
-                self.oled.line(x_1+length + 40, y_1+length + 40, x_2+length + 40, y_2+length + 40, 1)
-                self.oled.show()
