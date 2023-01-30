51adb2fff8776018d443cbe39b0641bf1fc3887d
Juan Cruz Pereyra Carrillo <62359758+Jety16@users.noreply.github.com>
2023-01-30 19:25:08 -0300

    Update index.js

--- clases/clase2/index.js
+++ clases/clase2/index.js
@@ -10,15 +10,3 @@ function concatValues(){
 
 
 console.log(concatValues())
-
-
-
-self._square(x_1 + + 28, y_1 , length)
-self._square(x_2 + + 28, y_2 , length)
-self.oled.line(x_2 + + 28, y_2 , x_2 + + 28, y_2 , 1)
-self.oled.line(x_1 + length + + 28, y_1 ,
-               x_2 + length + + 28, y_2 , 1)
-self.oled.line(x_1 + + 28, y_1 + length ,
-               x_2 + + 28, y_2 + length , 1)
-self.oled.line(x_1 + length + + 28, y_1 + length +
-               48, x_2 + length + + 28, y_2 + length , 1)
